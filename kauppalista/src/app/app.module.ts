d4e3b00a78b6632c460b1752e1c81cc204f31ca1
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-04-23 22:19:03 +0300

    Lista komponentin luonti

--- kauppalista/src/app/app.module.ts
+++ kauppalista/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
