9286b4f7086e06116e7c97b39a17cb12061ddcbf
LAMMINAHO\Petri <lammpe77@gmail.com>
2017-04-24 01:01:30 +0300

    Lista component mukaan sovellukseen ja näkyviin

--- kauppalista/src/app/app.component.ts
+++ kauppalista/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+//import {ListaComponent} from './lista/lista.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // tslint:disable-next-line:whitespace
 })
 export class AppComponent {
-  title = 'Tähän tulee kauppalista';
+  title = 'Kauppalista';
 }
